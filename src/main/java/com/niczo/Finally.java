531955f578bdd8e1cf457ab415f2f5ffea070f28
Li Nic <lhc19920627@sina.com>
2017-07-31 17:48:30 +0800

    finally使用,仅在对应的try执行后执行,在return执行之后

--- src/main/java/com/niczo/Finally.java
+++ src/main/java/com/niczo/Finally.java
@@ -7,4 +7,36 @@ package com.niczo;
  * DATE:2017/7/31
  */
 public class Finally {
+    public static void main(String[] args) {
+        System.out.println("return value of test(): " + test());
+        System.out.println("return value of test1():" + test1());
+    }
+
+    private static int test() {
+        int i = 1;
+
+        if(i == 1)
+            return 0;
+        System.out.println("the previous statement of try block");
+        i = i / 0;
+
+        try {
+            System.out.println("try block");
+            return i;
+        }finally {
+            System.out.println("finally block");
+        }
+    }
+
+    public static int test1() {
+        int i = 1;
+
+        try {
+            System.out.println("try block");
+            System.exit(0);
+            return i;
+        }finally {
+            System.out.println("finally block");
+        }
+    }
 }

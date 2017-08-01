d84c695f2f66b72685e40b9ad8a5e27327971a69
Li Nic <lhc19920627@sina.com>
2017-08-01 18:09:08 +0800

    finally使用,仅在对应的try执行后执行,在return执行之后

---
+++ src/main/java/com/niczo/MyClientSocket.java
@@ -0,0 +1,13 @@
+package com.niczo;
+
+/**
+ * DES:
+ * PROJECT:MyStudy
+ * Author:Li Nic
+ * DATE:2017/8/1
+ */
+public class MyClientSocket {
+    public static void main(String[] args) {
+
+    }
+}

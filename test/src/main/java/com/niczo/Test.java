bbc401e3a38f4dba80e36790c5644c808eebe7b5
李鸿程 <lihongcheng@qjdchina.com>
2021-04-20 15:28:30 +0800

    删除根目录下代码，全部采用module

---
+++ test/src/main/java/com/niczo/Test.java
@@ -0,0 +1,13 @@
+package com.niczo;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author lhc
+ */
+@Slf4j
+public class Test {
+    public static void main(String[] args) {
+        log.info("Hello World");
+    }
+}

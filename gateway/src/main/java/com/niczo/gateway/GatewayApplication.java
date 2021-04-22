98f1e351ff3ce26852a6ea8d18e00de16c05fc16
李鸿程 <lihongcheng@qjdchina.com>
2021-04-22 21:36:56 +0800

    网关项目

---
+++ gateway/src/main/java/com/niczo/gateway/GatewayApplication.java
@@ -0,0 +1,26 @@
+package com.niczo.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * @author lhc
+ */
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("path_route", r -> r.path("/boss2/*")
+                        .uri("https://qa8boss.qjdchina.com/"))
+                .build();
+    }
+}
